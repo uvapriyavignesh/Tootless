c47e8ce03741cb7a939811945ea8de5509a2d9e4
test <test>
2024-04-05 00:30:06 +0530

    test

--- RnnNetworkModel.py
+++ RnnNetworkModel.py
@@ -7,6 +7,9 @@ class RnnNetworkModel(nn.Module):
         self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
         self.dropout = nn.Dropout(dropout_prob)
         self.fc1 = nn.Linear(hidden_size, 100)
+        self.h0 = nn.Linear(100, 150)
+        self.h1 = nn.Linear(150, 150)
+        self.h2 = nn.Linear(150, 100)
         self.fc2 = nn.Linear(100, 50)
         self.fc3 = nn.Linear(50, output_size)
         self.relu = nn.ReLU()
@@ -14,10 +17,20 @@ class RnnNetworkModel(nn.Module):
 
     def forward(self, x):
         out, inut_layer = self.rnn(x)
-        out = self.dropout(out)
         out = self.relu(self.fc1(out[:, -1, :]))
+
         out = self.dropout(out)
+        out = self.relu(self.h0(out))
+        out = self.dropout(out)
+
+        out = self.dropout(out)
+        out = self.relu(self.h1(out))
+        out = self.dropout(out)
+
+        out = self.dropout(out)
+        out = self.relu(self.h2(out))
+        out = self.dropout(out)
+
         out = self.relu(self.fc2(out))
-        out = self.dropout(out)
         out = self.sigmoid(self.fc3(out))
         return out
